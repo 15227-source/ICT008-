da70b0d766cbcaba20f6d7776fac1a0293aadded
15227-source <15227@ait.nsw.edu.au>
2026-03-23 19:35:35 +1100

    Commit 1*
    Initial commit - basic Book class and display functionality

--- ICT008.cpp
+++ ICT008.cpp
@@ -1,20 +1,46 @@
-// ICT008.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <array>
 
-int main()
-{
-    std::cout << "Hello World hi!\n";
+using namespace std;
+
+class Book {
+private:
+    string title;
+    string author;
+    string isbn;
+
+public:
+    Book() {}
+
+    Book(string t, string a, string i) {
+        title = t;
+        author = a;
+        isbn = i;
+    }
+
+    void displayBook() const {
+        cout << "Title: " << title << endl;
+        cout << "Author: " << author << endl;
+        cout << "ISBN: " << isbn << endl;
+        cout << "-------------------------" << endl;
+    }
+};
+
+int main() {
+    array<Book, 5> books = {
+        Book("The Great Gatsby", "F. Fitzgerald", "ISBN001"),
+        Book("To Kill a Mockingbird", "Harper Lee", "ISBN002"),
+        Book("1984", "George Orwell", "ISBN003"),
+        Book("Pride and Prejudice", "Jane Austen", "ISBN004"),
+        Book("Moby Dick", "Herman Melville", "ISBN005")
+    };
+
+    cout << "Library Books:\n" << endl;
+
+    for (const auto& book : books) {
+        book.displayBook();
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
